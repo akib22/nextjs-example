9cffdcff687f56a52e04f2c94853878a7b97b2e4
akib22 <adnanakib75@gamil.com>
2020-11-23 23:08:24 +0600

    Configure app for build and deploy

--- pages/api/users.js
+++
@@ -1,6 +0,0 @@
-import { users } from "../../data/user.json";
-
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ users })
-};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: ['./src/**/*.html', './src/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       backgroundImage: (theme) => ({
-        profileBackground: "url('https://images.unsplash.com/photo-1522093537031-3ee69e6b1746?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a634781c01d2dd529412c2d1e2224ec0&auto=format&fit=crop&w=2098&q=80');",
+        profileBackground:
+          "url('https://images.unsplash.com/photo-1522093537031-3ee69e6b1746?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a634781c01d2dd529412c2d1e2224ec0&auto=format&fit=crop&w=2098&q=80');",
       }),
     },
   },
